cff0764e4743f71693f4258a8b4a3e3ec1ec06e3
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-06-20 12:34:52 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -15,15 +15,15 @@ from PIL import Image
 import argparse
 
 import predict_load_functions
-import train_save_functions
 
 #Command Line Arguments
 
 ap = argparse.ArgumentParser(
     description='predict-file')
-ap.add_argument('input_img', default='/home/workspace/ImageClassifier/flowers/test/10/image_07090.jpg', nargs='*', action="store", type = str)
-ap.add_argument('checkpoint', nargs='*', action="store",type = str)
+ap.add_argument('--input_img', default='/home/workspace/ImageClassifier/flowers/test/102/image_08004.jpg', action="store", type = str)
+ap.add_argument('--checkpoint',default='MASTER_CHECKPOINT.pth',action="store",type = str)
 ap.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
+ap.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
 ap.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
 ap.add_argument('--gpu', default="gpu", action="store", dest="gpu")
 
@@ -31,25 +31,16 @@ pa = ap.parse_args()
 path_image = pa.input_img
 number_of_outputs = pa.top_k
 power = pa.gpu
-input_img = pa.input_img
+category_val= pa.category_names
+learningr = pa.learning_rate
 path = pa.checkpoint
 
-
-print('Loading Data........')
-training_loader, testing_loader, validation_loader,train_datasets = train_save_functions.Load_Data()
-
 print('Loading Checkpoint....')
-model=predict_load_functions.load_checkpoint(path)
-
-
-with open('cat_to_name.json', 'r') as json_file:
-    cat_to_name = json.load(json_file)
+model=predict_load_functions.load_checkpoint(path,learningr)
 
 print('Predicting.......')
-top_probs, top_labels, top_flowers = predict_load_functions.predict(path_image, model, number_of_outputs, power)
+top_probs, top_labels, top_flowers = predict_load_functions.predict(category_val,path_image, model, number_of_outputs, power)
 
-print(top_flowers)
-print(top_probs)
 predict_load_functions.print_probability(top_flowers, top_probs)
 
 print("Here you are")
